7654e2610e6e907988ed3e70dc623b450316baa3
Garlos <garlos.daw@gmail.com>
2018-06-14 23:53:16 +0200

    Cleaned Header component

--- app/components/Layout/Header.js
+++ app/components/Layout/Header.js
@@ -3,20 +3,18 @@ import { StyleSheet, View } from 'react-native'
 import { Button, Icon, Title } from 'native-base'
 import PropTypes from 'prop-types'
 
-const getTitle = ({ descriptor }) => descriptor.options.title
-
-const Header = ({ navigation, scene }) => (
+const Header = ({ navigation, navigationOptions }) => (
   <View style={styles.header}>
     <Button rounded light transparent onPress={navigation.toggleDrawer} style={styles.button}>
       <Icon name="menu" />
     </Button>
-    <Title>{getTitle(scene)}</Title>
+    <Title>{navigationOptions.title}</Title>
   </View>
 )
 
 Header.propTypes = {
   navigation: PropTypes.object.isRequired,
-  scene: PropTypes.object
+  navigationOptions: PropTypes.object
 }
 
 export default Header
